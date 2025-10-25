cdce0b0aa6e713e166617c20fc06100f65fb9f4b
Lobbbey <joshuatylerbandy@gmail.com>
2025-10-25 14:21:27 -0400

    REmoved old frontend

--- backend/route/model.js
+++ backend/route/model.js
@@ -18,7 +18,23 @@ router.post('/add', async (req, res) => {
 // Desc: Remove media to database
 router.post('/delete', async (req, res) => {
     try {
-        const {} = req.body;
+        const { mediaId, userId } = req.body;
+
+        if( !mediaId || !userId){
+            return res.status(400).json({ message: "Missing mediaId or userId"});
+        }
+
+        const media = await Media.findById(mediaId);
+        if(!media) return res.status(400).json({ message: "Entry not found"});
+
+        if(media.user.toString() !== userId) return res.status(400).json({ message: "You do not have permission to delete this entry"});
+
+        await Media.deleteOne({ _id: mediaId });
+        
+        res.status(200).json({
+            success: true,
+            message: "Media removed successfully",
+        });
         
     } catch (error) {
         console.error('Error:', error);
